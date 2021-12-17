a62504a94c25990b63ce2142ed038ba957e001f3
munton <13339985102@163.com>
2021-12-17 13:00:50 +0800

    Site updated: 2021-12-17 13:00:49

---
+++ dist/music.js
@@ -0,0 +1,28 @@
+const ap = new APlayer({
+    container: document.getElementById('aplayer'),
+    volume: 0.3,
+    autoplay: true,
+    loop: 'all',
+    listFolded: true,
+    listMaxHeight: 60,
+    audio: [
+        // {
+        // // name: '千秋',
+        // // artist: '天予风流',
+        // // url: '/千秋.mp3', 本地存在source下
+        // // cover: '/images/天予风流.jpg'
+        // },
+        {
+        name: '等到烟火清凉',
+        artist: '许嵩',
+        url: 'https://www.ytmp3.cn/down/30350.mp3',
+        cover: '/images/烟火.jpg'
+        },
+        {
+        name: '千秋',
+        artist: '古剑奇谭3-天予风流',
+        url: 'https://xp.hifini.com:233/music/%E5%8F%A4%E5%89%91%E5%A5%87%E8%B0%AD3_%E5%8D%83%E7%A7%8B.m4a',
+        cover: '/images/天予风流.jpg'
+        }
+    ]
+});
