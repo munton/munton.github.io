cd91f4ca561f1f13ba6e1fe6c9f892e16c0261da
munton <13339985102@163.com>
2022-01-16 19:05:26 +0800

    Site updated: 2022-01-16 19:05:25

--- dist/music.js
+++ dist/music.js
@@ -17,16 +17,15 @@ const ap = new APlayer({
         },
         {
         name: '千秋',
-        artist: '古剑3 - 天予风流',
+        artist: '古剑3-天予风流',
         url: 'https://xp.hifini.com:233/music/%E5%8F%A4%E5%89%91%E5%A5%87%E8%B0%AD3_%E5%8D%83%E7%A7%8B.m4a',
         cover: '/images/天予风流.jpg'
         },
         {
         name: '绝代风华',
-        artist: '天下3 - 许嵩',
+        artist: '天下3-许嵩',
         url: '/juedaifenghua.mp3',
-        cover: '/images/xusong.png',
-        lrc: '/lrc/绝代风华 - 许嵩.lrc'
+        cover: '/images/xusong.png'
         }
     ]
 });
